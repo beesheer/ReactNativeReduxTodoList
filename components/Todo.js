aaee4a6e75bc1b45a305858a0b4408b0733adabe
Bin Xu <beesheer@gmail.com>
2018-08-30 14:06:51 -0400

    Style for the todo list.

--- components/Todo.js
+++ components/Todo.js
@@ -5,7 +5,14 @@ class Todo extends React.Component {
   render() {
     return (
       <Text
-        style={{ display: 'flex', textDecorationLine: this.props.isCompleted ? 'line-through' : 'none', fontSize: 30, borderBottomWidth: 1, flex: 1}}
+        style={{
+          display: 'flex', 
+          textDecorationLine: this.props.isCompleted ? 'line-through' : 'none', 
+          fontSize: 30, 
+          borderBottomWidth: 1, 
+          flex: 1, 
+          borderBottomColor: '#ececec' 
+        }}
         onPress={this.props.onPress} // Return a passed function from parent. It is in props.
       >
         {this.props.text}
